74ea28a137181b9e169cbaca0c34312a4c45c93b
nomadic121 <nomadic121@gmail.com>
2020-03-26 22:50:32 +0300

    create

---
+++ src/main/java/mimimimetr/dto/CatDto.java
@@ -0,0 +1,16 @@
+package mimimimetr.dto;
+
+import lombok.Data;
+
+@Data
+public class CatDto {
+
+    private Long id;
+
+    private String name;
+
+    private String image;
+
+    private String message;
+
+}
