d412f654c94de01bcf17097b131ca1baabf8e8db
nomadic121 <nomadic121@gmail.com>
2020-03-26 22:16:00 +0300

    create RegistrationForm

---
+++ src/main/java/mimimimetr/form/RegistrationForm.java
@@ -0,0 +1,25 @@
+package mimimimetr.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class RegistrationForm {
+
+    private String name;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    @NotBlank(message = "Password confirmation cannot be empty")
+    private String password2;
+
+    @Email(message = "Email is not correct")
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    private String roles;
+
+}
