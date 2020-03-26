6f11b8021fbb8f1a0bda796fdb0b076a273e3819
nomadic121 <nomadic121@gmail.com>
2020-03-26 22:45:54 +0300

    refactor

--- src/main/java/mimimimetr/controller/RegistrationController.java
+++ src/main/java/mimimimetr/controller/RegistrationController.java
@@ -1,8 +1,9 @@
 package mimimimetr.controller;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import mimimimetr.entity.User;
+import mimimimetr.entity.Role;
+import mimimimetr.entity.UserEntity;
+import mimimimetr.form.UserRegistrationForm;
 import mimimimetr.service.UserService;
 import mimimimetr.util.ControllerUtils;
 import org.springframework.stereotype.Controller;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class RegistrationController {
 
-    private final @NonNull
-    UserService userSevice;
+    private final UserService userSevice;
 
     @GetMapping("/registration")
     public String registration() {
@@ -27,20 +28,26 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(@Valid UserRegistrationForm userRegistrationForm, BindingResult bindingResult, Model model) {
+        if (userRegistrationForm.getPassword() != null && !userRegistrationForm.getPassword().equals(userRegistrationForm.getPassword2())) {
             model.addAttribute("passwordError", "Passwords are different!");
         }
 
         if (bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
-
             model.mergeAttributes(errors);
-
             return "registration";
         }
 
-        if (!userSevice.addUser(user)) {
+        UserEntity userEntity = UserEntity.builder()
+                .name(userRegistrationForm.getName())
+                .password(userRegistrationForm.getPassword())
+                .email(userRegistrationForm.getEmail())
+//                .roles(Collections.singleton(Role.valueOf(registrationForm.getRoles())))
+                .roles(Collections.singleton(Role.USER))
+                .build();
+
+        if (!userSevice.addUser(userEntity)) {
             model.addAttribute("usernameError", "User exists!");
             return "registration";
         }
@@ -48,10 +55,10 @@ public class RegistrationController {
         return "redirect:/login";
     }
 
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
 //    @GetMapping("/activate/{code}")
 //    public String activate(Model model, @PathVariable String code) {
