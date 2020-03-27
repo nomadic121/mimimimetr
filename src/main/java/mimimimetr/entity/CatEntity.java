fd0005bfd1c9840452b83430413c2b29dbdcf410
nomadic121 <nomadic121@gmail.com>
2020-03-27 08:45:50 +0300

    add uniqueConstraints

--- src/main/java/mimimimetr/entity/CatEntity.java
+++ src/main/java/mimimimetr/entity/CatEntity.java
@@ -5,16 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name", "image"})
+})
 public class CatEntity {
 
     @Id
